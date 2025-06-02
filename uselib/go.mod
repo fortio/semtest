50b7d6fc476ed63a7d8d99427711f5402b14e0ef
Laurent Demailly <ldemailly@gmail.com>
2025-06-02 13:26:40 -0700

    sample using the tlib

---
+++ uselib/go.mod
@@ -0,0 +1,5 @@
+module github.com/fortio/semtest/uselib
+
+go 1.24.3
+
+require github.com/fortio/semtest/v2 v2.1.0
---
+++ uselib/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/fortio/semtest/v2/tlib"
+)
+
+func main() {
+	tlib.DoSomething()
+}
