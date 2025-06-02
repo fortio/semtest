7ea4379667771ea714845f02a905a9ae9b338f34
Laurent Demailly <ldemailly@gmail.com>
2025-06-02 13:42:12 -0700

    Use the retracted version and print buildinfo

--- uselib/go.mod
+++ uselib/go.mod
@@ -2,4 +2,7 @@ module github.com/fortio/semtest/uselib
 
 go 1.24.3
 
-require github.com/fortio/semtest/v2 v2.1.0
+require (
+	fortio.org/version v1.0.4
+	github.com/fortio/semtest/v2 v2.1.0
+)
--- uselib/main.go
+++ uselib/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+
+	"fortio.org/version"
+
 	"github.com/fortio/semtest/v2/tlib"
 )
 
 func main() {
+	_, _, fullV := version.FromBuildInfo()
+	fmt.Print(fullV)
 	tlib.DoSomething()
 }
